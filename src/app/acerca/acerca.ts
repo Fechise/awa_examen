32916d95bd3a8bbebb7bcb0152bb862fe3f7c188
Andrés Mateo Quillupangui Osorio <andres.quillupangui@epn.edu.ec>
2025-11-26 18:03:26 -0500

    First commit.

---
+++ src/app/acerca/acerca.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-acerca',
+  imports: [RouterLink],
+  templateUrl: './acerca.html',
+  styleUrl: './acerca.css',
+})
+export class Acerca {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Acerca } from './acerca/acerca';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {path: 'home', component: Home, title: 'Home'},
+    {path: 'acerca', component: Acerca, title: 'Acerca'}
+];
